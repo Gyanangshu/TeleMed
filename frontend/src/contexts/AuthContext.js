8130a97d6b11ae49c64b6b367bbf1dfc7446141f
Gyanangshu <babubiku@gmail.com>
2025-04-04 21:10:22 +0530

    latest

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -65,21 +65,18 @@ export const AuthProvider = ({ children }) => {
     isAuthenticated: !!user
   };
 
-  if (loading) {
-    return null;
-  }
+  // if (loading) {
+  //   return null;
+  // }
 
   return (
-    <AuthContext.Provider value={{value}}>
+    <AuthContext.Provider value={value}>
       {children}
     </AuthContext.Provider>
   )
 };
 
 export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-  return context;
+  return useContext(AuthContext);
+
 }; 
