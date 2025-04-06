e7b88bf1c542a2ecb3480039f80b4bab977369ed
Gyanangshu <babubiku@gmail.com>
2025-04-06 15:28:56 +0530

    registration navigation on login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from '../../utils/axios';
 import { connectSocket } from '../../utils/socket';
 
@@ -95,6 +95,10 @@ export default function Login() {
             >
               Sign in
             </button>
+
+            <Link to="/register" className="text-sm text-gray-500 hover:text-gray-700">
+              Don't have an account? Register
+            </Link>
           </div>
         </form>
       </div>
