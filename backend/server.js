50f8e089855fce6ebf479f233d934815ab1675ff
Gyanangshu <babubiku@gmail.com>
2025-07-24 15:58:58 +0530

    fix: CORS

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 // CORS configuration
 const corsOptions = {
-  origin: ['https://telemedicine-webapp.netlify.app', 'http://localhost:5173', 'http://localhost:3000'],
+  origin: ['https://telemedhealthcare.netlify.app/', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
